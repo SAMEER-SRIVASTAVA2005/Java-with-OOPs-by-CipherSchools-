0e9624b770e1c0158a028d908313d2d36bb4465b
Sameer Srivastava <sameersrivastav83033@gmail.com>
2025-06-22 14:07:37 +0530

    day 3

---
+++ 1-Java Fundamentals/2-Variables_Datatypes/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args) {
+
+        int a = 5;
+        float b=33.5f;
+        double c=44.5;
+
+        System.out.println(c);
+
+    }
+}
---
+++ 1-Java Fundamentals/3-Type_conversion/typeConversion.java
@@ -0,0 +1,15 @@
+
+public class typeConversion {
+    public static void main(String... args) {
+        // Implicit conversiion
+        // int x = 5;
+        // double y = x;
+        // System.out.println(y);
+        
+        
+        // Explicit conversiion
+        double x=55.77;
+        int y=(int)x;  
+        System.out.println(y);
+    }
+}
