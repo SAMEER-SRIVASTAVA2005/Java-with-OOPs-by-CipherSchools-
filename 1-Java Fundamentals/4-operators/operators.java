b804a2a3776ae350ef10cfd65d6fd578ff136e5c
Sameer Srivastava <sameersrivastav83033@gmail.com>
2025-06-22 14:09:15 +0530

    day 4

---
+++ 1-Java Fundamentals/4-operators/operators.java
@@ -0,0 +1,32 @@
+// package 1-Java Fundamentals.4-operators;
+
+public class operators {
+   public static void main(String...args){
+    
+    // arithmetic 
+        int num1=5;
+        int num2=10;
+
+        // int ans=num1+num2;
+        // int ans=num1-num2;
+        // int ans=num1*num2;
+        // int ans=num1/num2;
+        int ans=num1%num2;
+
+        System.out.println(ans);
+
+    // inc and dec operator
+        int x=7;
+
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+
+
+        System.out.println(x--);
+        System.out.println(x);
+        System.out.println(--x);
+
+        
+   } 
+}
---
+++ 1-Java Fundamentals/4-operators/operators2.java
@@ -0,0 +1,33 @@
+// package 1-Java Fundamentals.4-operators;
+
+public class operators2 {
+    public static void main(String[] args) {
+        int num1=5;
+        int num2=10;
+
+        System.out.println(num2++ + ++num2);
+
+
+        // ---------------------------
+        // relational operator
+        
+        int x=4,y=7;
+        
+        System.out.println(x==y);
+        System.out.println(x>=y);
+        System.out.println(x<=y);
+        System.out.println(x<y);
+        System.out.println(x>y);
+        System.out.println(x!=y);
+        
+
+        // ---------------------------
+        // logical operator && || !
+
+
+        // ---------------------------
+        // assignment operator = += -= *= /= 
+        System.out.println(x%=y);
+
+    }
+}
